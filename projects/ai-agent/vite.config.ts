4f6dbf58e32f5f32ab15bf298ae40d800924fdee
zhangkai <781226151@qq.com>
2025-03-25 08:45:37 +0800

    feat:修改

--- projects/ai-agent/vite.config.ts
+++ projects/ai-agent/vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),  // 将 @ 映射到 src 目录
     },
   },
+  server: {
+    proxy: {
+      '/api': {//获取路径中包含了/api的请求
+        target: 'http://localhost:8080',//后台服务所在的源
+        changeOrigin: true,//修改源
+        rewrite: (path) => path.replace(/^\/api/, '')///api替换为''
+      }
+    }
+  }
 })
