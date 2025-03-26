de304125eab18503321e4a46c6869c71e8ed3d26
panweiji <406798106@qq.com>
2025-03-26 09:21:07 +0800

    feat: JavaScript技术栈单仓多包代码架构

--- projects-demo/antd-antv/src/App.tsx
+++ projects-demo/antd-antv/src/App.tsx
@@ -1,12 +1,9 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
-import { Button, Segmented } from 'antd'
+import { Segmented } from 'antd'
 import { Chart } from '@antv/g2'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   // 准备数据
   const data = [
@@ -31,7 +28,7 @@ function App() {
     chart.data(data)
     chart
       .area()
-      .encode('x', (d) => d.date)
+      .encode('x', (d: { date: string }) => d.date)
       .encode('y', 'value')
       .encode('shape', 'smooth') // 'area', 'smooth', 'hvh', 'vh', 'hv'
       .encode('color', 'type')
@@ -60,22 +57,6 @@ function App() {
         <div id="container"></div>
         {/*  <Button type="primary">Button</Button>*/}
       </div>
-      {/*      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>*/}
     </>
   )
 }
